e44b0fed6b251a9ab9189060dbf073f3765b2d74
Mason Adsero <madsero1@gmail.com>
2023-02-09 23:53:40 -0800

    Added a simple map for putting an excercise in the db

--- lib/excercise_model.dart
+++ lib/excercise_model.dart
@@ -1,9 +1,14 @@
 class Excercise {
+  int id;
   String name;
   String desc;
   String? video;
   String? image;
   List<int> progress = [];
   List<String> progressTimes = [];
-  Excercise(this.name, this.desc, [this.video, this.image]);
+  Excercise(this.id, this.name, this.desc, [this.video, this.image]);
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'name': name, 'desc': desc};
+  }
 }
