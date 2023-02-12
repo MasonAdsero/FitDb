8a3046c74c437064ee5fe03a0bae3c41e24e5216
joshuapalicka <55159564+joshuapalicka@users.noreply.github.com>
2023-02-12 12:42:52 -0800

    Adds YouTube video player package. It is only partially implemented so far.

--- lib/display_exercise.dart
+++ lib/display_exercise.dart
@@ -2,14 +2,22 @@ import 'package:fit_db_project/exercise_model.dart';
 import 'package:fit_db_project/video_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import 'create_exercise.dart';
 
 class ExerciseView extends StatelessWidget{
-  const ExerciseView({super.key, required this.currentExercise});
+  ExerciseView({super.key, required this.currentExercise});
   final Exercise currentExercise;
 
-
+  static String myVideoId = "IODxDxX7oi4";
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: myVideoId,
+    flags: const YoutubePlayerFlags(
+      autoPlay: false,
+      mute: false,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +40,14 @@ class ExerciseView extends StatelessWidget{
       body: Column(
         children: [
           Text(currentExercise.desc),
-          Text(currentExercise.video ?? ""),
-          if(hasVideo)
-            SizedBox(
-              height: 500,
-              width: 500,
-              child: VideoPlayerScreen(link: currentExercise.video ?? "")
-            )
+          //Text(currentExercise.video ?? ""),
+          YoutubePlayer(controller: _controller),
+          //if(hasVideo)
+            //SizedBox(
+            //  height: 500,
+            //  width: 500,
+            //  child: VideoPlayerScreen(link: currentExercise.video ?? "")
+            //)
 
           ]
       )
