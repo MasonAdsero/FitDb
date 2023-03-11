11573ebf0bc5beea55010d4ba17e403a4228ac00
Mason Adsero <madsero1@gmail.com>
2023-03-11 10:33:04 -0800

    setting up mock

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,13 +6,21 @@ import 'package:fit_db_project/exercise_provider.dart';
 import 'package:fit_db_project/db_provider.dart';
 import 'package:fit_db_project/sqflite_db.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+extension WithScaffold on WidgetTester {
+  pumpWithScaffold(Widget widget) async =>
+      pumpWidget(MaterialApp(home: Scaffold(body: widget)));
+}
+
+@GenerateMocks([FitDatabase])
 void main() {
   setUp(() async {
     WidgetsFlutterBinding.ensureInitialized();
   });
 
-  test("Database insert works as intended", () async {
+  /*test("Database insert works as intended", () async {
     final db = FitDatabase('test_insert.db');
     await db.openDB();
 
@@ -31,7 +39,7 @@ void main() {
     expect(exerciseList.length, 2);
 
     db.deleteDatabase();
-  });
+  });*/
 
   testWidgets('Home screen displays exercises', (WidgetTester tester) async {
     var exerciseOne = Exercise(1, "sit-ups", "10 sit-ups in one minute");
@@ -49,5 +57,10 @@ void main() {
                 create: (context) => exerciseProvider,
                 child: MyHomePage(title: 'FitDB')))));
     expect(find.byType(ListTile), findsNWidgets(2));
+    expect(find.byType(BottomAppBar), findsOneWidget);
+  });
+
+  testWidgets('Exercise form displays all fields', (widgetTester) async {
+    final exerciseProvider = ExerciseList([]);
   });
 }
